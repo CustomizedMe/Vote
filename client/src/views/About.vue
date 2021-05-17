f17c8d21592f587a88fbd2997d75a966cf1dd06c
CustomizedMe <45433611+CustomizedMe@users.noreply.github.com>
2021-05-17 17:56:10 +0530

    gitignore

---
+++ client/src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <voteButton />
+    <VoteList />
+  </div>
+</template>
+<script>
+import voteButton from "../components/voteButton"
+import VoteList from "../components/VoteList.vue"
+export default {
+  name: "About",
+  components: { voteButton, VoteList },
+}
+</script>
